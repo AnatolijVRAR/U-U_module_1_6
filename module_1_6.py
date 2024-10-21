3180722964e26bb75eea3885e24cc2986b205dda
AnatolijVRAR <63042317+AnatolijVRAR@users.noreply.github.com>
2024-10-21 19:43:55 +0900

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+# Словари
+print("2")
+my_dict = {'Felix': 2017, 'Lucas': 2019, 'Tolik': 1988}
+print(my_dict)
+print(my_dict ['Felix'])
+print(my_dict.get('Alik'))
+my_dict.update({'Yuliy': 1987, 'Alik': 1988})
+my_dict.pop('Tolik')
+print(my_dict)
+# Множества
+print("3")
+my_set = {2, 2, "cat", "dog", 3.4, 3.5, 3.6, 3.5}
+print(my_set)
+print(my_set.update({6, 88}))
+print(my_set.remove(3.5))
+print(my_set)
